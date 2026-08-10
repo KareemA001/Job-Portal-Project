45689e5add3266afc6b064b15b9bca430a06ed60
KareemA001 <kalaa8851@gmail.com>
2026-08-10 12:05:25 +0300

    feat: implementing aop for method execution monitoring

---
+++ job-portal-backend/src/main/java/com/spring/job_portal_backend/aspects/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.spring.job_portal_backend.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(* com.spring.job_portal_backend..*.*(..))")
+    public Object logAndMeasureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        String methodName = joinPoint.getSignature().toShortString();
+        Object[] methodArgs = joinPoint.getArgs();
+        log.info("➡️ Entering method: {}", methodName);
+        log.info("📥 Arguments: {}", Arrays.toString(methodArgs));
+        // Proceed with actual business method
+        Object result = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - startTime;
+        log.info("✅ Method executed successfully: {}", methodName);
+        log.info("⏱ Execution time: {} ms", executionTime);
+        return result;
+    }
+}
