d804576b0745664bc71586fdb247ce429fbe6327
KareemA001 <kalaa8851@gmail.com>
2026-09-07 15:32:52 +0300

    feat: configure HTTP service groups for third-party APIs

--- job-portal-backend/src/main/java/com/spring/job_portal_backend/client/config/HttpServiceClientConfig.java
+++ job-portal-backend/src/main/java/com/spring/job_portal_backend/client/config/HttpServiceClientConfig.java
@@ -3,10 +3,39 @@ package com.spring.job_portal_backend.client.config;
 
 import com.spring.job_portal_backend.client.service.PostService;
 import com.spring.job_portal_backend.client.service.TodoService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration
-@ImportHttpServices(types = {TodoService.class, PostService.class})
+@ImportHttpServices(group = "todos", types = {TodoService.class})
+@ImportHttpServices(group = "posts", types = {PostService.class})
 public class HttpServiceClientConfig {
+
+    @Bean
+    public RestClientHttpServiceGroupConfigurer groupConfigurer() {
+
+        return groups -> {
+            groups.filterByName("todos").forEachClient(
+                    (group, restClientBuilder) -> {
+                        restClientBuilder.baseUrl("https://jsonplaceholder.typicode.com/todos")
+                                .requestInterceptor((request, body, execution) -> {
+                                    //request.getHeaders().setBearerAuth("Bearer Token");
+                                    return execution.execute(request, body);
+                                }).build();
+                    }
+            );
+            groups.filterByName("posts").forEachClient(
+                    (group, restClientBuilder) -> {
+                        restClientBuilder.baseUrl("https://jsonplaceholder.typicode.com/posts")
+                                .defaultHeader("Accept","application/json")
+                                .requestInterceptor((request, body, execution) -> {
+                                    //request.getHeaders().setBearerAuth("Bearer Token");
+                                    return execution.execute(request, body);
+                                }).build();
+                    }
+            );
+        };
+    }
 }
